f511a8d409a4c750af2f86593b6b1ff4538aa5f4
viernes <luis.muro@gmail.com>
2018-10-12 16:06:18 -0500

    Ajustes finales

---
+++ 10.0/entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:=${POSTGRES_HOST:='db'}}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${PGPASSWORD:=${DB_ENV_POSTGRESSYS_PASSWORD:=${POSTGRESSYS_PASSWORD:='syspass'}}}
+: ${CREATEUSER:=${DB_ENV_CREATEUSER:=${CREATEUSER:='yes'}}}
+
+if [ "$CREATEUSER" == "yes" ]; then
+	echo $USER
+	createuser -h $HOST -U postgres -d -l $USER
+fi
+
+
+set -e
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if ! grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
+        DB_ARGS+=("--${param}")
+        DB_ARGS+=("${value}")
+   fi;
+}
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
+
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            exec odoo "$@"
+        else
+		if [ -f /var/lib/odoo/odoo.conf ] ; then
+			echo "Ejecutando /var/lib/odoo/odoo.conf"
+			exec odoo "$@" "${DB_ARGS[@]}" --db-filter "$USER" --config /var/lib/odoo/odoo.conf
+		else
+			echo "Ejecutando /etc/odoo/odoo.conf"
+        		exec odoo "$@" "${DB_ARGS[@]}" --db-filter "$USER"
+		fi
+
+        fi
+        ;;
+    -*)
+	if [ -f /var/lib/odoo/odoo.conf ] ; then
+		echo "Ejecutando /var/lib/odoo/odoo.conf"
+		exec odoo "$@" "${DB_ARGS[@]}" --db-filter "$USER" --config /var/lib/odoo/odoo.conf
+	else
+		echo "Ejecutando /etc/odoo/odoo.conf"
+        	exec odoo "$@" "${DB_ARGS[@]}" --db-filter "$USER"
+	fi
+        ;;
+    *)
+        exec "$@"
+esac
+
+exit 1
